20a242d80f9b0f235dfd5ef2d748198352a10314
Spacegod007 <spacegod007@outlook.com>
2018-05-08 14:45:19 +0200

    added Id to User and Implemented UserDAOJPAImpl using JPA instead of a collection

--- src/main/java/auction/dao/UserDAOJPAImpl.java
+++ src/main/java/auction/dao/UserDAOJPAImpl.java
@@ -2,14 +2,17 @@ package auction.dao;
 
 import auction.domain.User;
 
-import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserDAOJPAImpl implements UserDAO {
+    private static final Logger LOGGER = Logger.getLogger(UserDAOJPAImpl.class.getName());
 
 //    private HashMap<String, User> users;
 
@@ -22,38 +25,186 @@ public class UserDAOJPAImpl implements UserDAO {
 
     @Override
     public int count() {
-//        return users.size();
+        int result = 0;
+
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        try
+        {
+            result = count(entityManager);
+            entityManager.getTransaction().commit();
+        }
+        catch (Exception e)
+        {
+            entityManager.getTransaction().rollback();
+            LOGGER.log(Level.SEVERE, "Something went wrong while interacting with the database", e);
+        }
+        finally
+        {
+            entityManager.close();
+        }
+
+        return result;
+    }
+
+    private int count(EntityManager entityManager)
+    {
+        Query query = entityManager.createNamedQuery("User.count", User.class);
+        return (Integer) query.getSingleResult();
     }
 
     @Override
     public void create(User user) {
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        try
+        {
+            create(entityManager, user);
+            entityManager.getTransaction().commit();
+        }
+        catch (Exception e)
+        {
+            entityManager.getTransaction().rollback();
+            LOGGER.log(Level.SEVERE, "Something went wrong while interacting with the database", e);
+        }
+        finally
+        {
+            entityManager.close();
+        }
+
 //         if (findByEmail(user.getEmail()) != null) {
 //            throw new EntityExistsException();
 //        }
 //        users.put(user.getEmail(), user);
     }
 
+    private void create(EntityManager entityManager, User user)
+    {
+        entityManager.persist(user);
+    }
+
     @Override
-    public void edit(User user) {
+    public void edit(User user)
+    {
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        try
+        {
+            edit(entityManager, user);
+            entityManager.getTransaction().commit();
+        }
+        catch (Exception e)
+        {
+            entityManager.getTransaction().rollback();
+            LOGGER.log(Level.SEVERE, "Something went wrong while interacting with the database", e);
+        }
+        finally
+        {
+            entityManager.close();
+        }
 //        if (findByEmail(user.getEmail()) == null) {
 //            throw new IllegalArgumentException();
 //        }
 //        users.put(user.getEmail(), user);
     }
 
+    private void edit(EntityManager entityManager, User user)
+    {
+        entityManager.merge(user);
+    }
+
 
     @Override
-    public List<User> findAll() {
+    public List<User> findAll()
+    {
+        List<User> result = new ArrayList<>();
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        try
+        {
+            result = findAll(entityManager);
+            entityManager.getTransaction().commit();
+        }
+        catch (Exception e)
+        {
+            entityManager.getTransaction().rollback();
+            LOGGER.log(Level.SEVERE, "Something went wrong while interacting with the database");
+        }
+        finally
+        {
+            entityManager.getTransaction().commit();
+        }
+        return result;
 //        return new ArrayList<User>(users.values());
     }
 
-    @Override
-    public User findByEmail(String email) {
-//        return users.get(email);
+    private List<User> findAll(EntityManager entityManager)
+    {
+        return entityManager.createNamedQuery("User.getAll", User.class)
+                .getResultList();
     }
 
     @Override
-    public void remove(User user) {
+    public User findByEmail(String email)
+    {
+        User result = null;
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        try
+        {
+            result = findByEmail(entityManager, email);
+            entityManager.getTransaction().commit();
+        }
+        catch (Exception e)
+        {
+            entityManager.getTransaction().rollback();
+            LOGGER.log(Level.SEVERE, "Something went wrong while interacting with the database", e);
+        }
+        finally
+        {
+            entityManager.close();
+        }
+        return result;
+//        return users.get(email);
+    }
+
+    private User findByEmail(EntityManager entityManager, String email)
+    {
+        return entityManager.createNamedQuery("User.findByEmail", User.class)
+                .setParameter("email", email)
+                .getSingleResult();
+    }
+
+    @Override
+    public void remove(User user)
+    {
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        try
+        {
+            remove(entityManager, user);
+            entityManager.getTransaction().commit();
+        }
+        catch (Exception e)
+        {
+            entityManager.getTransaction().rollback();
+            LOGGER.log(Level.SEVERE, "Something went wrong while interacting with the database", e);
+        }
+        finally
+        {
+            entityManager.close();
+        }
 //        users.remove(user.getEmail());
+    }
+
+    private void remove(EntityManager entityManager, User user)
+    {
+        entityManager.remove(entityManager.merge(user));
     }
 }
--- src/main/java/auction/domain/User.java
+++ src/main/java/auction/domain/User.java
@@ -1,15 +1,48 @@
 package auction.domain;
 
+import javax.persistence.*;
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "User.count", query = "SELECT COUNT(u) FROM User AS u"),
+        @NamedQuery(name = "User.getAll", query = "SELECT u FROM User AS u"),
+        @NamedQuery(name = "User.findByEmail", query = "SELECT u FROM User AS u WHERE u.email = :email")
+})
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     private String email;
 
+    public User() {}
+
     public User(String email) {
         this.email = email;
-
     }
 
     public String getEmail() {
         return email;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        return email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = id;
+        result = 31 * result + email.hashCode();
+        return result;
     }
 }
